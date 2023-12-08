3c49128017d83416aedf796fb3ba975d74b5f571
EveraldoTorres1984 <torresjr1984@hotmail.com>
2023-12-08 10:43:37 -0300

    Projeto finalizado

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+import os
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///comunidade.db'
+app.config["SECRET_KEY"] = '60e5454207df9e1af0ffa8bb11a147ac'
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
+
+from fakepinterest import routes
